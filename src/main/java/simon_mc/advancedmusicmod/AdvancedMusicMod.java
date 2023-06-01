aa303bf006c092b1a504263bbe5e3b070fe68d77
simon-mc28 <hartkopfmoritz5@gmail.com>
2023-06-01 21:12:05 +0200

    Initial commit

---
+++ src/main/java/simon_mc/advancedmusicmod/AdvancedMusicMod.java
@@ -0,0 +1,40 @@
+package simon_mc.advancedmusicmod;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.CreativeModeTabEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+@Mod(simon_mc.advancedmusicmod.AdvancedMusicMod.MOD_ID)
+public class AdvancedMusicMod {
+
+    public static final String MOD_ID = "advancedmusicmod";
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public AdvancedMusicMod() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        modEventBus.addListener(this::commonSetup);
+
+        MinecraftForge.EVENT_BUS.register(this);
+
+        modEventBus.addListener(this::addCreative);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event) {}
+
+    private void addCreative(CreativeModeTabEvent.BuildContents event) {}
+
+    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ClientModEvents {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event) {}
+    }
+}
