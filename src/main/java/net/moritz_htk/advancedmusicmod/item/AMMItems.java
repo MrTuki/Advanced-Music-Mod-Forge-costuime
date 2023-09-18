da53998096c9397a743c5e911b6a4ae83bcd7406
simon-mc28 <hartkopfmoritz5@gmail.com>
2023-09-18 17:21:25 +0200

    Fixed bugs

--- src/main/java/net/moritz_htk/advancedmusicmod/item/AMMItems.java
+++ src/main/java/net/moritz_htk/advancedmusicmod/item/AMMItems.java
@@ -1,5 +1,6 @@
 package net.moritz_htk.advancedmusicmod.item;
 
+import net.minecraft.sounds.SoundEvent;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Rarity;
 import net.minecraft.world.item.RecordItem;
@@ -13,20 +14,25 @@ import net.moritz_htk.advancedmusicmod.sounds.AMMSoundEvents;
 public class AMMItems {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, AdvancedMusicMod.MOD_ID);
 
-    public static final RegistryObject<Item> MUSIC_DISC_AUGUST = ITEMS.register("music_disc_august", () -> new RecordItem(8, AMMSoundEvents.MUSIC_DISC_AUGUST, new Item.Properties().stacksTo(1).rarity(Rarity.RARE), 3820));
-    public static final RegistryObject<Item> MUSIC_DISC_CATTAILS = ITEMS.register("music_disc_cattails", () -> new RecordItem(8, AMMSoundEvents.MUSIC_DISC_CATTAILS, new Item.Properties().stacksTo(1).rarity(Rarity.RARE), 3120));
-    public static final RegistryObject<Item> MUSIC_DISC_CHERRY_BLOSSOM = ITEMS.register("music_disc_cherry_blossom", () -> new RecordItem(8, AMMSoundEvents.MUSIC_DISC_CHERRY_BLOSSOM, new Item.Properties().stacksTo(1).rarity(Rarity.RARE), 2540));
-    public static final RegistryObject<Item> MUSIC_DISC_DEATH_OF_KINGS = ITEMS.register("music_disc_death_of_kings", () -> new RecordItem(8, AMMSoundEvents.MUSIC_DISC_DEATH_OF_KINGS, new Item.Properties().stacksTo(1).rarity(Rarity.RARE), 2860));
-    public static final RegistryObject<Item> MUSIC_DISC_HEDWIG = ITEMS.register("music_disc_hedwig", () -> new RecordItem(8, AMMSoundEvents.MUSIC_DISC_HEDWIG, new Item.Properties().stacksTo(1).rarity(Rarity.RARE), 3500));
-    public static final RegistryObject<Item> MUSIC_DISC_JUST_IN_TIME = ITEMS.register("music_disc_just_in_time", () -> new RecordItem(8, AMMSoundEvents.MUSIC_DISC_JUST_IN_TIME, new Item.Properties().stacksTo(1).rarity(Rarity.RARE), 4040));
-    public static final RegistryObject<Item> MUSIC_DISC_LAPSE = ITEMS.register("music_disc_lapse", () -> new RecordItem(8, AMMSoundEvents.MUSIC_DISC_LAPSE, new Item.Properties().stacksTo(1).rarity(Rarity.RARE), 2340));
-    public static final RegistryObject<Item> MUSIC_DISC_MIDNIGHT_TALE = ITEMS.register("music_disc_midnight_tale", () -> new RecordItem(8, AMMSoundEvents.MUSIC_DISC_MIDNIGHT_TALE, new Item.Properties().stacksTo(1).rarity(Rarity.RARE), 3060));
-    public static final RegistryObject<Item> MUSIC_DISC_PAIN = ITEMS.register("music_disc_pain", () -> new RecordItem(8, AMMSoundEvents.MUSIC_DISC_PAIN, new Item.Properties().stacksTo(1).rarity(Rarity.RARE), 3480));
-    public static final RegistryObject<Item> MUSIC_DISC_QUIET = ITEMS.register("music_disc_quiet", () -> new RecordItem(8, AMMSoundEvents.MUSIC_DISC_QUIET, new Item.Properties().stacksTo(1).rarity(Rarity.RARE), 2960));
-    public static final RegistryObject<Item> MUSIC_DISC_SCREEN_SAVER = ITEMS.register("music_disc_screen_saver", () -> new RecordItem(8, AMMSoundEvents.MUSIC_DISC_SCREEN_SAVER, new Item.Properties().stacksTo(1).rarity(Rarity.RARE), 3520));
-    public static final RegistryObject<Item> MUSIC_DISC_SLEEP = ITEMS.register("music_disc_sleep", () -> new RecordItem(8, AMMSoundEvents.MUSIC_DISC_SLEEP, new Item.Properties().stacksTo(1).rarity(Rarity.RARE), 4060));
-    public static final RegistryObject<Item> MUSIC_DISC_TABUK = ITEMS.register("music_disc_tabuk", () -> new RecordItem(8, AMMSoundEvents.MUSIC_DISC_TABUK, new Item.Properties().stacksTo(1).rarity(Rarity.RARE), 3120));
-    public static final RegistryObject<Item> MUSIC_DISC_WITH_REGARDS = ITEMS.register("music_disc_with_regards", () -> new RecordItem(8, AMMSoundEvents.MUSIC_DISC_WITH_REGARDS, new Item.Properties().stacksTo(1).rarity(Rarity.RARE), 3500));
+    public static final RegistryObject<Item> MUSIC_DISC_AUGUST = registerRecordItem("music_disc_august", AMMSoundEvents.MUSIC_DISC_AUGUST, 191);
+    public static final RegistryObject<Item> MUSIC_DISC_CATTAILS = registerRecordItem("music_disc_cattails", AMMSoundEvents.MUSIC_DISC_CATTAILS, 156);
+    public static final RegistryObject<Item> MUSIC_DISC_CHERRY_BLOSSOM = registerRecordItem("music_disc_cherry_blossom", AMMSoundEvents.MUSIC_DISC_CHERRY_BLOSSOM, 127);
+    public static final RegistryObject<Item> MUSIC_DISC_DEATH_OF_KINGS = registerRecordItem("music_disc_death_of_kings", AMMSoundEvents.MUSIC_DISC_DEATH_OF_KINGS, 143);
+    public static final RegistryObject<Item> MUSIC_DISC_HEDWIG = registerRecordItem("music_disc_hedwig", AMMSoundEvents.MUSIC_DISC_HEDWIG, 175);
+    public static final RegistryObject<Item> MUSIC_DISC_JUST_IN_TIME = registerRecordItem("music_disc_just_in_time", AMMSoundEvents.MUSIC_DISC_JUST_IN_TIME, 202);
+    public static final RegistryObject<Item> MUSIC_DISC_LAPSE = registerRecordItem("music_disc_lapse", AMMSoundEvents.MUSIC_DISC_LAPSE, 117);
+    public static final RegistryObject<Item> MUSIC_DISC_MIDNIGHT_TALE = registerRecordItem("music_disc_midnight_tale", AMMSoundEvents.MUSIC_DISC_MIDNIGHT_TALE, 153);
+    public static final RegistryObject<Item> MUSIC_DISC_PAIN = registerRecordItem("music_disc_pain", AMMSoundEvents.MUSIC_DISC_PAIN, 174);
+    public static final RegistryObject<Item> MUSIC_DISC_QUIET = registerRecordItem("music_disc_quiet", AMMSoundEvents.MUSIC_DISC_QUIET, 148);
+    public static final RegistryObject<Item> MUSIC_DISC_SCREEN_SAVER = registerRecordItem("music_disc_screen_saver", AMMSoundEvents.MUSIC_DISC_SCREEN_SAVER, 176);
+    public static final RegistryObject<Item> MUSIC_DISC_SLEEP = registerRecordItem("music_disc_sleep", AMMSoundEvents.MUSIC_DISC_SLEEP, 203);
+    public static final RegistryObject<Item> MUSIC_DISC_TABUK = registerRecordItem("music_disc_tabuk", AMMSoundEvents.MUSIC_DISC_TABUK, 156);
+    public static final RegistryObject<Item> MUSIC_DISC_WITH_REGARDS = registerRecordItem("music_disc_with_regards", AMMSoundEvents.MUSIC_DISC_WITH_REGARDS, 175);
+
+    private static RegistryObject<Item> registerRecordItem(String name, RegistryObject<SoundEvent> event, int lengthInSeconds) {
+        int lengthInTicks = (int) (lengthInSeconds / 0.05);
+        return ITEMS.register(name, () -> new RecordItem(9, event, new Item.Properties().stacksTo(1).rarity(Rarity.RARE), lengthInTicks));
+    }
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
